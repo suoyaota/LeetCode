338a9ff3e82764c3d7d80b8ee24196487e0b0a8d
unknown <769670936@qq.com>
2019-10-21 21:22:34 +0800

    20191021

---
+++ P88.java
@@ -0,0 +1,32 @@
+/**
+ * @author William Wang
+ * @date 2019/10/21 21:18
+ */
+public class P88 {
+    class Solution {
+        /**
+         *@Date 2019/10/21 21:18
+         *@Author William Wang
+         *@Desicription Runtime & Memory Usage: 100.00%
+         *              简单归并
+         */
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int i = 0;
+            int j = 0;
+            int[] ans = new int[m + n];
+            for (int t = 0; t < m + n;t++) {
+                if (j == n || i != m && nums1[i] < nums2[j]) {
+                    // nums2用完了或者 nums1没用完并且nums1的数小
+                    ans[t] = nums1[i];
+                    i++;
+                } else {
+                    ans[t] = nums2[j];
+                    j++;
+                }
+            }
+            for (int x = 0; x < m + n; x++) {
+                nums1[x] = ans[x];
+            }
+        }
+    }
+}
