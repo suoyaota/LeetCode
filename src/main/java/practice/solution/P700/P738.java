3b7909ec15e813eebf0ea3da351ca9b53624d434
suoyaota <769670936@qq.com>
2025-08-28 15:25:59 +0800

    P738

--- src/main/java/practice/solution/P700/P738.java
+++ src/main/java/practice/solution/P700/P738.java
@@ -2,6 +2,8 @@ package practice.solution.P700;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 /**
  * @Author WilliamWang
  * @Date 2025/8/27-15:18
@@ -9,4 +11,32 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class P738 {
+
+    // 暴力
+    public int[] dailyTemperatures1(int[] temperatures) {
+        // 存储温度在输入中的下标
+        int[] idxOfT = new int[101];
+        int len = temperatures.length;
+        int[] ans = new int[len];
+        Arrays.fill(idxOfT, Integer.MAX_VALUE);
+        // 从输入末尾开始遍历寻找答案
+        for (int i = len - 1; i >= 0; i--) {
+            int t = temperatures[i];
+            // 最近更高温度下标
+            int minWormerIdx = Integer.MAX_VALUE;
+            // 寻找比它高的温度是否已在数组末尾出现
+            // 因为温度的范围比输入长度小，因此可以减少时间
+            for (int j = t + 1; j < 101; j++) {
+                int idx = idxOfT[j];
+                if (idx != Integer.MAX_VALUE) {
+                    minWormerIdx = Math.min(minWormerIdx, idx);
+                }
+            }
+            if (minWormerIdx != Integer.MAX_VALUE) {
+                ans[i] = minWormerIdx - i;
+            }
+            idxOfT[t] = i;
+        }
+        return ans;
+    }
 }
---
+++ src/main/java/practice/solution/common/Useful.java
@@ -0,0 +1,16 @@
+package practice.solution.common;
+
+import java.util.Arrays;
+
+/**
+ * @Author WilliamWang
+ * @Date 2025/8/28-08:54
+ */
+
+
+public class Useful {
+
+   // Arrays.fill(idxOfT, Integer.MAX_VALUE);
+
+
+}
